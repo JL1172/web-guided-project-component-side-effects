9dfd835b1065c9398a9a03ddbbcaae456dcff1dc
Jacob Lang <jacoblang72@comcast.net>
2023-07-26 21:49:52 -0400

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // 👉 TASK 1 - import the axios lib from node_modules
-
+import axios from 'axios'
 // 👉 TASK 2 - import the contants from constants/index.js
-
+import { BASE_URL, API_KEY } from '../constants';
 import Details from './Details'
 import Friend from './Friend';
 
 export default function App() {
   const [friends, setFriends] = useState([])
   const [currentFriendId, setCurrentFriendId] = useState(null)
+
 
   const openDetails = id => {
     setCurrentFriendId(id)
@@ -17,16 +18,47 @@ export default function App() {
   const closeDetails = () => {
     setCurrentFriendId(null)
   }
-
+  console.log('this will render before any effect does')
   // 👉 TASK 3 - make an effect that runs after FIRST DOM surgery
   // caused by the first render only. You'll need `useEffect` from React.
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
 
+  /* useEffect(()=> {
+     async function getData() {
+     const res = await axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
+     try {
+       setFriends(res.data)
+     } catch {
+       console.log(err)
+     }
+     }
+     getData(); 
+   },[])*/
+
+  ///or
+  useEffect(() => {
+    function getData() {
+      axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
+        .then((res) => {
+          setFriends(res.data)
+          console.log(res.data)
+        })
+        .catch((e) => {
+          console.error(e)
+        })
+    }
+    getData();
+  }, []);
   return (
     <div className='container'>
       <h1>Some of my friends:</h1>
       {/* start by mapping over the friends array...*/}
+      {
+        friends.map(fr=> {
+          return <Friend key = {fr.id} info = {fr} />
+        })
+      }
       {
         currentFriendId && <Details friendId={currentFriendId} close={closeDetails} />
       }
